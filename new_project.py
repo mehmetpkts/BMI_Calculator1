87c7d5a811ad765a3d779bbce530b81f200cfafc
Mehmet <mehmetpektasss18@gmail.com>
2023-07-29 19:17:27 +0300

    updated

--- new_project.py
+++ new_project.py
@@ -8,25 +8,36 @@ window.minsize(width=300,height=300)
 # funaction
 
 def funaction_button():
-    input1 = float(entry.get())
-    input2 = float(entry2.get())
-    new = input1**2 / 10000
-    sonuc = input2 / new
-    print(sonuc)
-    def siralama():
-        if sonuc < 18.5:
-            print("weak")
-        elif sonuc >= 18.5 and sonuc < 24.9 :
-            print("ideal weight")
-        elif sonuc >=24.9 and sonuc < 29.9:
-            print("overweight")
+    input1 = entry.get()
+    input2 = entry2.get()
+    try:
+        if input1 == "" or input2 == "":
+            label3.config(text="Please enter both weight and height")
         else:
-            print("obesity")
-    siralama()
-    
+            new = float(input1)**2 / 10000
+            sonuc = float(input2) / new
+    except:
+        label3.config(text="Please enter number")
+
+    if float(input1) < 0 or float(input2) < 0:
+        label3.config(text="please enter positive value")
+
+
+    def arrangement():
+        if sonuc < 18.5:
+            label3.config(text=f"your BMI is : {round(sonuc,2)} = weak")
+        elif sonuc >= 18.5 and sonuc < 24.9 :
+            label3.config(text=f"your BMI is : {round(sonuc,2)} = ideal weight")
+        elif sonuc >=24.9 and sonuc < 29.9:
+            label3.config(text=f"your BMI is : {round(sonuc,2)} = overweight")
+        else:
+            label3.config(text=f"your BMI is : {round(sonuc,2)} = obesitys")
+    arrangement()
+
+
 
 # question one
-label = tkinter.Label(text="Please enter your length(cm)")
+label = tkinter.Label(text="Please enter your height(cm)")
 label.config(bg="black",fg="white")
 label.pack()
 
@@ -49,6 +60,12 @@ entry2.pack()
 
 button = tkinter.Button(text="Click",command=funaction_button)
 button.pack()
+
+# write
+
+label3 = tkinter.Label(text="")
+label3.pack()
+
 
 #finish
 
